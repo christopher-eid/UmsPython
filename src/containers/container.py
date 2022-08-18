ab3b222b9d08d81e90ed42eb61f29e1cd44d0ee1
christopher <christopher.c.eid@outlook.com>
2022-08-18 23:32:11 +0300

    feature - add mongodb service

--- src/containers/container.py
+++ src/containers/container.py
@@ -5,8 +5,9 @@ from src.application.course.abstract_course_service import AbstractCourseService
 from src.persistence.repositories.course.course_db_service import CourseDBService
 from src.application.abstract_repositories.course.abstract_course_db_service import AbstractCourseDBService
 from src.persistence.connections.db_engine import DBEngine
-
-
+from src.persistence.mongo_repositories.course.course_mongo_service import CourseMongoService
+from src.application.abstract_mongo_repositories.course.abstract_course_mongo_service import AbstractCourseMongoService
+from src.persistence.connections.mongo_db_client import MongoDBClient
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
@@ -14,12 +15,22 @@ class Container(containers.DeclarativeContainer):
         DBEngine
     )
 
+    mongo_db_client = providers.Singleton(
+        MongoDBClient
+    )
+
     db_service = providers.Factory(
         AbstractCourseDBService.register(CourseDBService),
         engine=db_engine
     )
 
+    mongo_db_service = providers.Factory(
+        AbstractCourseMongoService.register(CourseMongoService),
+        mongo_db_client = mongo_db_client
+    )
+
     course_service = providers.Factory(
         AbstractCourseService.register(CourseService),
-        db=db_service
+        db=db_service,
+        mongo_db = mongo_db_service
     )
