e2e3a13a9e162b3e344c4a3f81fb1278da52a40a
christopher <christopher.c.eid@outlook.com>
2022-08-22 10:27:11 +0300

    feature - implement add and get students with excel, cqrs included

--- src/application/student_cqrs/queries/get_students_excel/get_students_excel_service.py
+++ src/application/student_cqrs/queries/get_students_excel/get_students_excel_service.py
@@ -6,6 +6,7 @@ import shutil
 from pathlib import Path
 from numpy import random
 from fastapi.responses import FileResponse
+from threading import Timer
 
 '''
 how to return downloadable files:
@@ -24,17 +25,23 @@ class GetStudentsExcelService(AbstractGetStudentsExcelService):
         destination_path = os.getcwd()
         random_name = random.randint(1, 1000000)
         string_name = str(random_name) + ".xlsx"
-        string_dest_path = os.path.join(destination_path, "src", "application", "stored_files", "hello.xlsx")
+        string_dest_path = os.path.join(destination_path, "src", "application", "stored_files", string_name)
         dest_path = Path(string_dest_path)
         students_dataframe.to_excel(dest_path)
 
-        '''we are returning the excel file that we created and created a background task so our file is deleted on our server after it has been returned
+        '''we are returning the excel file that we created and created a timer so our file is deleted on our server after it has been returned
         
             -file response will read from a specific path when called and return the file read
             -path.unlink is used to delete the file from a certain path
             -background is to apply it after returning the file
             -we should dest_path.unlink WITHOUT THE PARANTHESES, or it will be called before returning file'''
-        file_res = FileResponse(path=string_dest_path, filename=string_dest_path,
-                                background=dest_path.unlink and print("File " + string_name + " removed"))
+
+        file_res = FileResponse(path=string_dest_path, filename=string_dest_path)
+        s = Timer(10.0, dest_path.unlink)
+
+        s.start()
+        #
+        # background = dest_path.unlink
+        # print("File " + string_name + " removed")
 
         return file_res
