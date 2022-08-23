e3c426bebf3ffa4478825714db473064eb30f43a
christopher <christopher.c.eid@outlook.com>
2022-08-23 11:23:43 +0300

    feature - implement middleware logging

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.presentation.ums_web_api.routers import courses
+
+
+#for middleware with logger to display time for each request
+#pip install python-time to be able to import time , PYTHON-TIME NOT TIME
+
+import time
+import logging
+logging.basicConfig(format='[%(asctime)s %(levelname)s]:%(message)s', level=logging.INFO)
+#to disable uvicorn standard loggers
+uvicorn_error = logging.getLogger("uvicorn.error")
+uvicorn_error.disabled = True
+uvicorn_access = logging.getLogger("uvicorn.access")
+uvicorn_access.disabled = True
+
 
 '''
 Setting up and starting up the app:
@@ -30,7 +44,14 @@ app = FastAPI()
 
 app.include_router(courses.router)
 
+@app.middleware("http")
+async def log_process_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
 
+    logging.info(" endpoint url:" + str(request.url) + " - process time: " + str(process_time) + "seconds")
+    return response
 
 #
 # @app.post("/add_course")
